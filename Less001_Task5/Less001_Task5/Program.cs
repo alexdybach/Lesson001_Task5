380320a5e349c099e6b67a3035b65fa076d06444
Олександр Дибач <alex.dybach.2014@i.ua>
2020-10-27 13:38:21 +0200

    Done lesson 1 task 5

---
+++ Less001_Task5/Less001_Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Less001_Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first_name = "Дибач";
+            string last_name = "Олександр";
+            string surname = "Миколайович";
+           // Console.WriteLine("First: {0} Last name: {1} Surname: {2}",first_name,last_name,surname);
+            Console.WriteLine($"First: {first_name} Last name: {last_name} Surname: {surname}");
+            
+        }
+    }
+}
